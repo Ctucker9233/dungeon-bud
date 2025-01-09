84515bb00371f178ed885e93dd9b62f1c4671518
Courtney Tucker <tuckersfurniture@gmail.com>
2025-01-08 17:20:10 -0800

    bind to specific host

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
+  app.listen(PORT, '0.0.0.0', function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
